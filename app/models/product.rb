65fff511006c7c85794ccd628d5bb35428daa34c
Paul J Solomon <psolomon@enova.com>
2012-03-11 22:01:44 -0500

    Iteration G2: Atom Feeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   has_many :line_items
+  has_many :orders, through: :line_items
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
@@ -21,5 +22,4 @@ class Product < ActiveRecord::Base
         return false
       end
     end
-
 end
